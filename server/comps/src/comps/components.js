ffd4f0b8be4d6d4b0a2160d5e409bc8e0db7047c
broberts2 <broc.roberts@outlook.com>
2020-01-30 10:49:49 -0700

    changes

--- server/comps/src/comps/components.js
+++ server/comps/src/comps/components.js
@@ -1,9 +1,14 @@
 import React from "react";
 import VideoLoop from "./video_loop/video_loop";
+import VideoButton from "./video_loop/video_loop";
 
 export default arg => {
   switch (arg.match.params.id) {
     case "VideoLoop":
       return <VideoLoop />;
+    case "VideoButton1":
+      return (
+        <VideoLoop src={require("./video_button/videos/abstract_blue.mp4")} />
+      );
   }
 };
