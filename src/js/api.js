1b195991b65d19768683fb03a0557801c923d31d
broberts2 <broc.roberts@outlook.com>
2020-03-02 18:21:11 -0700

    auto

--- src/js/api.js
+++ src/js/api.js
@@ -1,21 +1,21 @@
 const production = false;
 
-const serverPath = production
+globals.constants.serverPath = production
   ? "https://titan-esports.org:8000"
   : "http://localhost:8000";
 
-const titanDraftPath = production
+globals.constants.titanDraftPath = production
   ? "https//titan-esports.org:7001"
   : "http://localhost:7001";
 
-const _t = async prm =>
+globals.fns._t = async prm =>
   await new Promise(async (resolve, reject) => {
     setTimeout(() => resolve({ code: 500, msg: "Request timed out." }), 8000);
     const response = await prm;
     resolve(response);
   });
 
-const _get = async (url, data) => {
+globals.fns._get = async (url, data) => {
   const titan_key = globals.fns.readTitanKey();
   const response = await fetch(url, {
     method: "GET",
@@ -33,7 +33,7 @@ const _get = async (url, data) => {
   return response.json().then(res => res);
 };
 
-const _post = async (url, data) => {
+globals.fns._post = async (url, data) => {
   const titan_key = globals.fns.readTitanKey();
   const response = await fetch(url, {
     method: "POST",
@@ -48,26 +48,9 @@ const _post = async (url, data) => {
 };
 
 // API CALLS
-globals.api.getDraftLogos = data =>
-  _t(_get(`${serverPath}/getDraftLogos`, data));
-
-globals.api.validateToken = () => _t(_get(`${serverPath}/s/validateToken`));
-
-globals.api.loginUser = data => _t(_get(`${serverPath}/loginUser`, data));
-
-globals.api.getAllUsers = data => _t(_get(`${serverPath}/getAllUsers`, data));
-
-globals.api.createDraft = data =>
-  _t(_post(`${titanDraftPath}/api/createDraft`, data));
-
-globals.api.saveGameToDatabase = data =>
-  _t(_post(`${titanDraftPath}/s/saveGameToDatabase`, data));
-
-globals.api.getPlayerStatsByLolId = data =>
-  _t(_get(`${titanDraftPath}/s/getPlayerStatsByLolId`, data));
-
-globals.api.getGameStatsByCode = data =>
-  _t(_get(`${titanDraftPath}/s/getGameStatsByCode`, data));
+let script = document.createElement("script");
+script.src = "js/api_calls.js";
+document.head.append(script);
 
 // AUTHENTICATOR
 (async () => {
