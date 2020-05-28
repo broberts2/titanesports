7b6e88c4189052ddc6c3426968f7278f710e19ea
broberts2 <broc.roberts@outlook.com>
2020-05-28 01:56:20 -0600

    changes

--- titan_draft/logo_index.js
+++ titan_draft/logo_index.js
@@ -4,100 +4,92 @@ const server = config.production
   : "http://localhost:7001";
 
 module.exports = {
-  admins: {
-    0: `${server}/WDGP.png`,
-    1: `${server}/WDGP.png`
+  ttpis: {
+    0: `${server}/Today_the_Plan_is_Simple.png`,
+    1: `${server}/Today_the_Plan_is_Simple.png`,
   },
-  afg: {
-    0: `${server}/AFG.png`,
-    1: `${server}/AFG-trans.png`
+  rpg: {
+    0: `${server}/Rat_Pack_Gaming_Logo.png`,
+    1: `${server}/Rat_Pack_Gaming_Logo.png`,
   },
-  baka: {
-    0: `${server}/BAKA.png`,
-    1: `${server}/BAKA-trans.png`
-  },
-  baka_pre: {
-    0: `${server}/BAKA-PRESCHOOL.png`,
-    1: `${server}/BAKA-PRESCHOOL.png`
-  },
-  il: {
-    0: `${server}/IL.png`,
-    1: `${server}/IL-trans.png`
-  },
-  mm: {
-    0: `${server}/MM.png`,
-    1: `${server}/MM-trans.png`
-  },
-  name: {
-    0: `${server}/NAME.png`,
-    1: `${server}/NAME-trans.png`
-  },
-  leaf: {
-    0: `${server}/LEAF-ELITES.png`,
-    1: `${server}/LEAF-ELITES.png`
-  },
-  rinc: {
-    0: `${server}/RINC.png`,
-    1: `${server}/RINC-trans.png`
-  },
-  rk: {
-    0: `${server}/RAINBOW-KITTENZ.png`,
-    1: `${server}/RAINBOW-KITTENZ.png`
-  },
-  ng: {
-    0: `${server}/NOT-GREEDY.png`,
-    1: `${server}/NOT-GREEDY.png`
-  },
-  nfg: {
-    0: `${server}/NFG.png`,
-    1: `${server}/NFG.png`
-  },
-  sk: {
-    0: `${server}/SK.png`,
-    1: `${server}/SK.png`
-  },
-  sfb: {
-    0: `${server}/SFB.png`,
-    1: `${server}/SFB-trans.png`
-  },
-  rr: {
-    0: `${server}/RR-trans.png`,
-    1: `${server}/RR-trans.png`
-  },
-  sbd: {
-    0: `${server}/STOP-BANNING-DRAVEN.png`,
-    1: `${server}/STOP-BANNING-DRAVEN.png`
+  rn: {
+    0: `${server}/reignnew.png`,
+    1: `${server}/reignnew.png`,
   },
   syn: {
     0: `${server}/SYN.png`,
-    1: `${server}/SYN.png`
+    1: `${server}/SYN.png`,
   },
-  ntf: {
-    0: `${server}/NTF-GAMING.png`,
-    1: `${server}/NTF-GAMING.png`
+  unkown: {
+    0: `${server}/unkown.png`,
+    1: `${server}/unkown.png`,
   },
-  uc: {
-    0: `${server}/UNKNOWN-CHALLENGERS.png`,
-    1: `${server}/UNKNOWN-CHALLENGERS.png`
+  yok: {
+    0: `${server}/yeahok.png`,
+    1: `${server}/yeahok.png`,
   },
-  ddd: {
-    0: `${server}/DDD.png`,
-    1: `${server}/DDD.png`
-  }
-  // tk: {
-  //   0: `${server}/team_khyroe.png`,
-  //   1: `${server}/team_khyroe.png`
-  // },
-  // tm: {
-  //   0: `${server}/team_major.png`,
-  //   1: `${server}/team_major.png`
-  // },
-  // tb: {
-  //   0: `${server}/team_braer.png`,
-  //   1: `${server}/team_braer.png`
-  // },
-  // tp: {
-  //   0: `${server}/team_phort.png`,
-  //   1: `${server}/team_phort.png`
-  // }
+  sk2: {
+    0: `${server}/SK2.png`,
+    1: `${server}/SK2.png`,
+  },
+  glacialsherbert: {
+    0: `${server}/GlacialSherbertLogo.png`,
+    1: `${server}/GlacialSherbertLogo.png`,
+  },
+  lh: {
+    0: `${server}/lighthouse_ravens.png`,
+    1: `${server}/lighthouse_ravens.png`,
+  },
+  nfg: {
+    0: `${server}/nfg.png`,
+    1: `${server}/nfg.png`,
+  },
+  nobleconcern: {
+    0: `${server}/noble_concern.png`,
+    1: `${server}/noble_concern.png`,
+  },
+  rainbowkittenz: {
+    0: `${server}/rainbow_kittenz.png`,
+    1: `${server}/rainbow_kittenz.png`,
+  },
+  rawr: {
+    0: `${server}/rawr.png`,
+    1: `${server}/rawr.png`,
+  },
+  dbth: {
+    0: `${server}/dbth.png`,
+    1: `${server}/dbth.png`,
+  },
+  friedchicken: {
+    0: `${server}/fried_chicken.png`,
+    1: `${server}/fried_chicken.png`,
+  },
+  glacial: {
+    0: `${server}/glacial.png`,
+    1: `${server}/glacial.png`,
+  },
+  glacial_equ: {
+    0: `${server}/glacial_equ.png`,
+    1: `${server}/glacial_equ.png`,
+  },
+  kyn: {
+    0: `${server}/kyn.png`,
+    1: `${server}/kyn.png`,
+  },
+  apark: {
+    0: `${server}/apark.png`,
+    1: `${server}/apark.png`,
+  },
+  caf: {
+    0: `${server}/caf.png`,
+    1: `${server}/caf.png`,
+  },
+  chaos: {
+    0: `${server}/chaos.png`,
+    1: `${server}/chaos.png`,
+  },
+  deadly_robots: {
+    0: `${server}/deadly_robots.png`,
+    1: `${server}/deadly_robots.png`,
+  },
 };
