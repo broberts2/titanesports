40808cb7c97f0fc63ccd9992cd238b6e05426954
broberts2 <broc.roberts@outlook.com>
2020-05-31 03:46:08 -0600

    changes

--- oracle/js/commands/help.js
+++ oracle/js/commands/help.js
@@ -8,7 +8,7 @@ module.exports = (client) => ({
   exec: () =>
     "!Help does not exist. Perhaps you were looking for\n\n```?help```",
   help: (() => {
-    let str = "--- Commands Index ---";
+    let str = "--------------- Commands Index ---------------";
     for (key in _help) {
       str += `${`\n\n${key}:\n`}${
         "```" + _help[key].map((el) => `!${el}`).join("\n") + "```"
