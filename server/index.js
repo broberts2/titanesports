e2e7536a13a36e3ee84305398eeb30a7ce526207
broberts2 <broc.roberts@outlook.com>
2020-05-31 00:02:36 -0600

    changes

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ if (config.production) {
     "/etc/letsencrypt/live/titan-esports.org/cert.pem",
     "utf8"
   );
-  server = http.createServer({ key, cert }, app);
+  server = http.createServer({ key, cert, rejectUnauthorized: false }, app);
 } else {
   server = http.createServer(app);
 }
