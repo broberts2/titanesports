cd78920c8f56d14b502ad33874095b9eefe73c1a
broberts2 <broc.roberts@outlook.com>
2020-05-31 01:13:37 -0600

    changes

--- oracle/js/commands/createcodes.js
+++ oracle/js/commands/createcodes.js
@@ -4,18 +4,26 @@ const titan_key = require("../../config").titan_key;
 
 module.exports = (client, roles) => ({
   exec: async (command) => {
-    const res = await fetch(
-      `${webserver}/s/createTournamentCode?team1=${command.args[0]}&team2=${command.args[1]}&weekNum=${command.args[2]}&seasonNum=${command.args[3]}&league=${command.args[4]}&n=${command.args[5]}`,
-      {
-        method: "GET",
-        headers: {
-          titan_key,
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }
-    ).then((_res) => _res.json());
-    return res.codes;
+    if (!(command.args.length < 5 || command.args.length > 6)) {
+      const res = await fetch(
+        `${webserver}/s/createTournamentCode?team1=${command.args[0]}&team2=${command.args[1]}&weekNum=${command.args[2]}&seasonNum=${command.args[3]}&league=${command.args[4]}&n=${command.args[5]}`,
+        {
+          method: "GET",
+          headers: {
+            titan_key,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((_res) => _res.json());
+      return `Code generation successful! Here you go:\n\n${res.codes.join(
+        "\n"
+      )}`;
+    } else {
+      return new Error(
+        "Hmm, I wasn't able to parse your arguments. You should probably get with a staff member for help or use:\n```?createcodes```."
+      );
+    }
   },
   help:
     "!createcodes - Creates a set of riot api game codes.\n\n```!createcodes <team_1> <team_2> <week_num> <season_num> <league> <series_size>```\nValid inputs:\nTeam 1: <team_name>\nTeam 2: <team_name>\nWeek Num: <number>\nSeason Num: <number>\nLeague: gladiator, olympian\nSeries Size (optional): 1, 2, 3, 4, 5, 6, 7",
