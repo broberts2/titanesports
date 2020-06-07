3036655b8521789ea27b5c5dc5022236dfdea392
broberts2 <broc.roberts@outlook.com>
2020-06-07 03:34:45 -0600

    changes

--- oracle/js/_embed.js
+++ oracle/js/_embed.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 
-module.exports = obj =>
+module.exports = (obj) =>
   new Discord.MessageEmbed()
     .setColor(obj.status)
     .setTitle("Oracle")
     .setDescription(obj.description)
     .setThumbnail(
-      "https://i.pinimg.com/originals/70/eb/0b/70eb0b53eb57c91db403928c5d02a19a.gif
+      "https://i.pinimg.com/originals/70/eb/0b/70eb0b53eb57c91db403928c5d02a19a.gif"
     )
     // .addFields(
     //   { name: "Regular field title", value: "Some value here" },
