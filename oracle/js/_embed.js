15eddf1f4d2b2769832513594d26c15f049ac3c2
broberts2 <broc.roberts@outlook.com>
2020-06-07 03:36:30 -0600

    changes

--- oracle/js/_embed.js
+++ oracle/js/_embed.js
@@ -5,9 +5,7 @@ module.exports = (obj) =>
     .setColor(obj.status)
     .setTitle("Oracle")
     .setDescription(obj.description)
-    .setThumbnail(
-      "https://i.pinimg.com/originals/70/eb/0b/70eb0b53eb57c91db403928c5d02a19a.gif"
-    )
+    .setThumbnail("https://i.gifer.com/XDZT.gif")
     // .addFields(
     //   { name: "Regular field title", value: "Some value here" },
     //   { name: "\u200B", value: "\u200B" }
