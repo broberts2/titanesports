69c1129d32e9ed2364b2414cb5f5dc18b339b86d
broberts2 <broc.roberts@outlook.com>
2021-06-13 20:46:53 -0600

    changes

--- server/controllers/admin/Applications.js
+++ server/controllers/admin/Applications.js
@@ -1,12 +1,19 @@
 const Applications = require("../../models/Applications");
+const Oracle = require("./Oracle");
 
 module.exports = {
-  post: async (req) =>
-    await Applications.create({
+  post: async (req) => {
+    const res = await Applications.create({
       JSON: req.body.JSON,
       origin: req.body.origin,
       category: req.body.category,
-    }),
+    });
+    await Oracle.OracleUtils.SendMessage({
+      channel: "853821461285044224",
+      message: `A new form in '${req.body.category}' was submitted.`,
+    });
+    return res;
+  },
   get: async (req) => await Applications.find({ category: req.query.category }),
   delete: async (req) => await Applications.remove({ _id: req.body.id }),
 };
