e92f603269209863b7b8b326481529e540543ca1
broberts2 <broc.roberts@outlook.com>
2021-06-29 14:55:04 -0600

    changes

--- server/OBS/events/startOBS.js
+++ server/OBS/events/startOBS.js
@@ -1,6 +1,7 @@
 const _ = require("../_utils");
 
 module.exports = async (exec, socket) => {
+	console.log("startOBS");
 	const _utils = _(exec);
 	const running = await _utils.isRunning();
 	_utils.focus();
